8786421dee6106e0104d47f5bc06d9e5c7399355
eliheadley <eliheadley@gmail.com>
2024-10-08 20:31:33 -0400

    Split scanner.cpp into a main and LexicalAnalyzer class

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -6,84 +6,82 @@
 #include <vector>
 
 enum TokenType {
-    T_AND,
-    T_ASSIGN,
-    T_BOOLTYPE,
-    T_BREAK,
-    T_CHARCONSTANT,
-    T_COMMA,
-    T_COMMENT,
-    T_CONTINUE,
-    T_DIV,
-    T_DOT,
-    T_ELSE,
-    T_EQ,
-    T_EXTERN,
-    T_FALSE,
-    T_FOR,
-    T_FUNC,
-    T_GEQ,
-    T_GT,
-    T_ID,
-    T_IF,
-    T_INTCONSTANT,
-    T_INTTYPE,
-    T_LCB,
-    T_LEFTSHIFT,
-    T_LEQ,
-    T_LPAREN,
-    T_LSB,
-    T_LT,
-    T_MINUS,
-    T_MOD,
-    T_MULT,
-    T_NEQ,
-    T_NOT,
-    T_NULL,
-    T_OR,
-    T_PACKAGE,
-    T_PLUS,
-    T_RCB,
-    T_RETURN,
-    T_RIGHTSHIFT,
-    T_RPAREN,
-    T_RSB,
-    T_SEMICOLON,
-    T_STRINGCONSTANT,
-    T_STRINGTYPE,
-    T_TRUE,
-    T_VAR,
-    T_VOID,
-    T_WHILE,
-    T_WHITESPACE,
+    T_AND,              // &&
+    T_ASSIGN,           // =
+    T_BOOLTYPE,         // bool
+    T_BREAK,            // break
+    T_CHARCONSTANT,     // char_lit (character literal)
+    T_COMMA,            // ,
+    T_COMMENT,          // comment
+    T_CONTINUE,         // continue
+    T_DIV,              // /
+    T_DOT,              // .
+    T_ELSE,             // else
+    T_EQ,               // ==
+    T_EXTERN,           // extern
+    T_FALSE,            // false
+    T_FOR,              // for
+    T_FUNC,             // func
+    T_GEQ,              // >=
+    T_GT,               // >
+    T_ID,               // identifier
+    T_IF,               // if
+    T_INTCONSTANT,      // int_lit (integer literal)
+    T_INTTYPE,          // int
+    T_LCB,              // {
+    T_LEFTSHIFT,        // <<
+    T_LEQ,              // <=
+    T_LPAREN,           // (
+    T_LSB,              // [
+    T_LT,               // <
+    T_MINUS,            // -
+    T_MOD,              // %
+    T_MULT,             // *
+    T_NEQ,              // !=
+    T_NOT,              // !
+    T_NULL,             // null
+    T_OR,               // ||
+    T_PACKAGE,          // package
+    T_PLUS,             // +
+    T_RCB,              // }
+    T_RETURN,           // return
+    T_RIGHTSHIFT,       // >>
+    T_RPAREN,           // )
+    T_RSB,              // ]
+    T_SEMICOLON,        // ;
+    T_STRINGCONSTANT,   // string_lit (string literal)
+    T_STRINGTYPE,       // string
+    T_TRUE,             // true
+    T_VAR,              // var
+    T_VOID,             // void
+    T_WHILE,            // while
+    T_WHITESPACE,       // whitespace
     Unknown
 };
 
-class LexicalAnalyzer {
-private:
-    int line_num;
-    int last_line_pos;
-    std::unordered_map<std::string, TokenType> keywords;
-    std::unordered_map<std::string, TokenType> operators;
-    std::unordered_map<std::string, TokenType> punctuation;
+class LexicalAnalyzer{
+    private:
+        std::unordered_map<std::string, TokenType> keywords;
+        std::unordered_map<std::string, TokenType> operators;
+        std::unordered_map<std::string, TokenType> punctuation;
 
-    void initKeywords();
-    void initOperators();
-    void initPunctuation();
-    bool isWhiteSpace(char c);
-    bool isLetter(char c);
-    bool isDigit(char c);
-    bool isPunctuation(char c);
-    bool isOperator(char c);
-    void removeComments(std::string* input);
-    int getColumnEnd(int col, std::string token);
-    std::string getNextWord(int* position, std::string* text, int* line, int* col);
-    std::string getType(TokenType type);
+        void initKeywords();
+        void initOperators();
+        void initPunctuation();
+        bool isWhiteSpace(char c);
+        bool isLetter(char c);
+        bool isDigit(char c);
+        bool isPunctuation(char c);
+        bool isOperator(char c);
+        void removeComments(std::string* input);
+        int getColumnEnd(int col, std::string token);
+        std::string getNextWord(int* position, std::string* text, int* line, int* col);
+        std::string getType(TokenType type);
 
-public:
-    LexicalAnalyzer(std::string* source);
-    void tokenize();
-    void printTokens();
+    public:
+        LexicalAnalyzer(std::string* source);
+        void tokenize();
+        void printTokens();
 };
 
-#endif // LEXICAL_ANALYZER_H
+#endif
