8f7942652c1aa1074c893cd3696dc78e0d68633f
eliheadley <eliheadley@gmail.com>
2024-10-14 21:21:33 -0400

    Deleted java files

--- test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test{
-    public static void main(String[] args){
-        String word = "word";
-        System.out.println("This is the substring: " + word.substring(0,1));
-        System.out.println("This is the string after: " + word);
-    }
-}
